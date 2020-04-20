13b50db8c0faef0e9dacb5835e7953378bccb336
trappettim <63984550+trappettim@users.noreply.github.com>
2020-04-20 14:01:05 +0800

    printing settings to see all the columns of the dataframe

--- Main_automobiles.py
+++ Main_automobiles.py
@@ -1,5 +1,14 @@
 import pandas as pd
 
+"""PRINTING SETTINGS FOR DATAFRAMES AND ARRAYS"""
+"""these instructions allow to see the all the columns of the dataframe on the run terminal.
+To print all the rows use the method: print(main_df.to_string())"""
+
+desired_width = 320
+pd.set_option('display.width', desired_width) #display width
+pd.set_option('display.max_columns',30) #number of columns
+#np.set_printoptions(linewidth=desired_width) #numpy set display width
+
 """IMPORTING DATASET INTO PANDAS DATAFRAME"""
 
 url = 'imports-85.data'
@@ -11,6 +20,7 @@ headers_names = ["symboling", "normalized-losses", "make", "fuel-type", "aspirat
 "city-mpg", "highway-mpg", "price"]
 # read csv, no headers in the file, put headers_names as headers, missing values identified by '?'
 main_df = pd.read_csv(url,header=None,names=headers_names,na_values='?')
-print(main_df.head())
+print(main_df.to_string())
+pass
 
 
