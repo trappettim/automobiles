64d3b0b8998179a699b885f0fa45573cb7c833ec
trappettim <63984550+trappettim@users.noreply.github.com>
2020-04-22 14:54:18 +0800

    Additions to missing values management

--- Main_automobiles.py
+++ Main_automobiles.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 """PRINTING SETTINGS FOR DATAFRAMES AND ARRAYS"""
+
 """these instructions allow to see the all the columns of the dataframe on the run terminal.
 To print all the rows use the method: print(main_df.to_string())"""
 
@@ -17,7 +18,7 @@ headers_names = ["symboling", "normalized-losses", "make", "fuel-type", "aspirat
                  "num-of-doors", "body-style", "drive-wheels", "engine-location",
                  "wheel-base", "length", "width", "height", "curb-weight",
                  "engine-type", "num-of-cylinders", "engine-size", "fuel-system",
-                 "bore", "stroke", "compression-ratio", "hoursepower", "peak-rpm",
+                 "bore", "stroke", "compression-ratio", "horsepower", "peak-rpm",
                  "city-mpg", "highway-mpg", "price"]
 
 # reads csv, no headers in the file, puts headers_names as headers, missing values identified by '?':
@@ -26,17 +27,51 @@ main_df = pd.read_csv(url, header=None, names=headers_names, na_values='?')
 """print(main_df.columns)"""  # to print the headers
 """print(main_df.to_string())"""  # to print the entire dataframe
 
+"""FINDING MISSING VALUES"""
+
+#  to replace '?' with NaN if we hadn't done before
+"""main_df.replace("?", np.nan, inplace = True)"""
+
+# to print the rows where specified column is NaN
+"""print(main_df.loc[main_df['normalized-losses'].isnull()])"""
+
+# to count the missing values in every column
+"""missing_values = main_df.isnull()  # new df where every value in the df is a boolean: True if NaN
+for column in missing_values.columns.values.tolist():  # loops on the list of columns names. tolist is to convert a-
+    print(column)                                      # -pandas series (df columns are series) into a python list
+    print (missing_values[column].value_counts())  # counts unique values in the column"""
+
+"""DROP THE WHOLE ROW WHERE MISSING VALUES IN IMPORTANT COLUMNS"""
+
+"""price is the column we want to predict. We can't have missing point here"""
+
 # drops rows with missing values in the column 'price'. Modifies the original df
 main_df.dropna(subset=['price'], inplace=True)
 
-# finds and prints the rows where the column 'normalized-losses' is NaN
-"""print(main_df.loc[main_df['normalized-losses'].isnull()])"""
+"""REPLACE MISSING VALUE WITH MEAN VALUES"""
 
-# replaces the NaN values in the 'normalizes-losses' column with the mean of the column
-mean = main_df['normalized-losses'].mean()
-main_df['normalized-losses'] = main_df['normalized-losses'].replace(np.nan, mean)
+Col_mean_val = ['normalized-losses','bore', 'stroke','horsepower','peak-rpm']
 
-# how to one-hot encode the fuel type column (turning categorical values into numerical ones)
+# replaces the NaN values in all the columns in Col_mean_val with the mean of the column
+for column in Col_mean_val:
+    mean = main_df[column].astype("float").mean(axis=0)
+    main_df[column].replace(np.nan, mean,inplace=True)
+
+"""REPLACE MISSING VALUE WITH FREQUENCY"""
+
+"""first method
+mode = main_df['num-of-doors'].mode()  # returns a pandas series with one element, the mode
+main_df['num-of-doors'].replace(np.nan, mode[0],inplace=True) # replaces NaN with the only element of the series mode
+"""
+
+"""second method"""
+mode = main_df['num-of-doors'].value_counts().idxmax() # value_counts for counting unique values. idxmax for mode
+main_df['num-of-doors'].replace(np.nan, mode,inplace=True)  # replaces NaN with the only element of the series mode
+
+print(main_df)
+print(main_df.dtypes)
+
+"""# how to one-hot encode the fuel type column (turning categorical values into numerical ones)
 dummy = pd.get_dummies(main_df['fuel-type'])  # creates a dummy object
 main_df = pd.concat([main_df, dummy], axis=1)  # concatenates the dummy object with the df
 
@@ -44,4 +79,4 @@ main_df = pd.concat([main_df, dummy], axis=1)  # concatenates the dummy object w
 bins = np.linspace(main_df['price'].min(),main_df['price'].max(),4) # creates np arrays with 4 equally spaced numbers
 group_names = ['Low','Medium','High'] # list with the label names
 main_df['price_binned'] = pd.cut(main_df['price'],bins,labels=group_names,include_lowest=True) # creates the new column
-print(main_df)
+print(main_df)"""
